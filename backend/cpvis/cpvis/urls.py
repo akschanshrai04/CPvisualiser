2c0d40434eaa304f6060ad3e5a5a84ae0d5cd130
opmkumar <sachinaniket2004@gmail.com>
2025-03-15 03:53:00 +0530

    backend: Add authentication.

--- backend/cpvis/cpvis/urls.py
+++ backend/cpvis/cpvis/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import include , path
+from django.urls import include, path
 
 urlpatterns = [
-    # path("cpmain/", include("cpmain.urls")),
-    path("" , include("cpmain.urls")),
     path("admin/", admin.site.urls),
+    path("api/", include("cpmain.urls")),
+    path("api/", include("djoser.urls")),
 ]
-
